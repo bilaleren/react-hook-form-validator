12bcdaee14daed6d3f55f7318deb58f3393a390a
webnoi <bilal@webnoi.com>
2023-07-09 16:20:28 +0300

    date format regexp fixed

--- src/utils/isDateString.ts
+++ src/utils/isDateString.ts
@@ -6,7 +6,7 @@ import isString from './isString'
  * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/datetime-local
  */
 const HTML_DATE_INPUT_FORMAT_REGEXP =
-  /^(?!0)\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[01])(T(0[0-9]|1[0-9]|2[1-3]):(0[0-9]|[1-5][0-9]))?$/
+  /^(?!0)\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[01])(T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]))?$/
 
 export function isDateString(value: unknown): value is string {
   return isString(value) && HTML_DATE_INPUT_FORMAT_REGEXP.test(value)
